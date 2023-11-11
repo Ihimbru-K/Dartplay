1463a9274ecd2429f69dbcec9f1051465eb9de3c
Ihimbru-k <kanihims25@gmail.com>
2023-11-11 22:03:39 +0100

    added .idea file

---
+++ .idea/Ex8_celsius_to_fahrenheit.dart
@@ -0,0 +1,11 @@
+
+// double CelciusToFahrenheit(int temp){
+//   return (temp *1.8)+32;
+// }
+
+double CelciusToFahrenheit(int temp) => temp*1.8+32;
+
+void main(){
+  print(CelciusToFahrenheit(10));
+
+}
